07e82b79f85fd0fd240cbce911cb986bd8cb4f1c
francis <richtofen@debian.backrooms.org>
2025-03-14 14:36:11 +0100

    daemon + server

--- src/daemon.c
+++ src/daemon.c
@@ -1,11 +1,21 @@
 #include "daemon.h"
 #include "syscall.h"
 #include "utils.h"
-
 #include <sys/file.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <poll.h>
 
 #define CLOSE_END 0
 #define NO_CLOSE_END 1
+#define MAX_CLIENTS 3
+
+static int my_htons(int port)
+{
+	return ((port & 0xff) << 8) | ((port & 0xff00) >> 8);
+}
 
 static void logger(const char *msg)
 {
@@ -16,6 +26,106 @@ static void logger(const char *msg)
 
 	_syscall(SYS_write, fd, msg, ft_strlen(msg));
 	_syscall(SYS_close, fd);
+}
+
+static int create_server(void)
+{
+	int fd = _syscall(SYS_socket, AF_INET, SOCK_STREAM, 0);
+	if (fd == -1) {
+		return -1;
+	}
+
+	struct sockaddr_in addr = {
+		.sin_family = AF_INET,
+		.sin_port = my_htons(8080),
+		.sin_addr.s_addr = INADDR_ANY
+	};
+
+	if (_syscall(SYS_bind, fd, &addr, sizeof(addr)) == -1) {
+		_syscall(SYS_close, fd);
+		return -1;
+	}
+
+	if (_syscall(SYS_listen, fd, 0) == -1) {
+		_syscall(SYS_close, fd);
+		return -1;
+	}
+
+	//if (_syscall(SYS_fcntl, fd, F_SETFL, O_NONBLOCK) == -1) {
+	//	_syscall(SYS_close, fd);
+	//	return -1;
+	//}
+
+	if (_syscall(SYS_setsockopt, fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) == -1) {
+		_syscall(SYS_close, fd);
+		return -1;
+	}
+
+	return fd;
+}
+
+static int accept_client(int fd)
+{
+	struct sockaddr_in addr;
+	socklen_t addr_len = sizeof(addr);
+
+	int client_fd = _syscall(SYS_accept, fd, &addr, &addr_len);
+	if (client_fd == -1) {
+		return -1;
+	}
+
+	return client_fd;
+}
+
+static void poller(int fd)
+{
+	struct pollfd fds[MAX_CLIENTS + 1];
+	fds[0].fd = fd;
+	fds[0].events = POLLIN;
+
+	for (int i = 1; i < MAX_CLIENTS + 1; i++) {
+		fds[i].fd = -1;
+	}
+
+	int use_client = 0;
+
+	while (1) {
+		if (_syscall(SYS_poll, &fds, use_client + 1, -1) == -1) {
+			break;
+		}
+
+		if (fds[0].revents & POLLIN) {
+			int client_fd = accept_client(fd);
+			if (client_fd != -1) {
+				for (int i = 1; i < MAX_CLIENTS + 1; i++) {
+					if (fds[i].fd == -1) {
+						fds[i].fd = client_fd;
+						fds[i].events = POLLIN;
+						use_client++;
+						logger((char []){"new client\n"});
+						break;
+					}
+				}
+			}
+		}
+
+		for (int i = 1; i < MAX_CLIENTS + 1; i++) {
+			if (fds[i].fd != -1 && fds[i].revents & POLLIN) {
+				char buf[1024];
+				int ret = _syscall(SYS_read, fds[i].fd, buf, sizeof(buf));
+				if (ret <= 0) {
+					_syscall(SYS_close, fds[i].fd);
+					fds[i].fd = -1;
+					use_client--;
+					logger((char []){"client disconnected\n"});
+				}
+				else {
+					buf[ret] = '\0';
+					logger(buf);
+				}
+			}
+		}
+	}
 }
 
 static int lock(int *lock_fd, int close_end)
@@ -65,19 +175,23 @@ static int unlock(int *lock_fd)
 
 void run(int *lock_fd)
 {
-	struct timespec ts = {5, 0};
-	while (1) {
 
-
-		logger((char []){"daemon is running\n"});
-
-		_syscall(SYS_nanosleep, &ts, 0);
-
+		//logger((char []){"daemon is running\n"});
+		//
+		//_syscall(SYS_nanosleep, &ts, 0);
+	
+	int server_fd = create_server();
+	if (server_fd == -1) {
+		return;
 	}
+
+	poller(server_fd);
+
+	_syscall(SYS_close, server_fd);
 
 	unlock(lock_fd);
 }
-//takes a function pointer in argument and calls it
+
 int	daemonize(void)
 {
 	int lock_fd = -1;
